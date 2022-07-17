76a3725fdbdd10e69b55cfac6be300483de6e6cd
Naomi Cole <naomisarahcole@gmail.com>
2022-07-17 14:31:03 +1000

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function eyesOnMe () {
+    function updateFocus(){
+        console.log("Good!")
+    }
+    function updateBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return <button onFocus={updateFocus} onBlur={updateBlur}>Eyes on me</button>
+
+
+    }
+
+export default eyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function keypad () {
+    function ChangeEvent(){
+        console.log("Entering password...")
+    }
+    return <input onChange={ChangeEvent} type="password" />
+}
+
+export default keypad;
